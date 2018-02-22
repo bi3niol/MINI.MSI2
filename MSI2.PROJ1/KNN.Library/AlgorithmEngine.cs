9eb2e435cf8d9f0555d132583cdae01f7dde95d7
Katarzyna Gołębiewska <staki777@wp.pl>
2018-02-22 13:30:37 +0100

    GetKNeighbors improvement

--- MSI2.PROJ1/KNN.Library/AlgorithmEngine.cs
+++ MSI2.PROJ1/KNN.Library/AlgorithmEngine.cs
@@ -7,71 +7,87 @@ using System.Threading.Tasks;
 
 namespace KNN.Library
 {
-    public sealed class AlgorithmEngine<T, TClassifier> where T : IMetricable<T>, IClassifiable<TClassifier>
-    {
-        /// <summary>
-        /// indicates norm
-        /// </summary>
-        public double P { get; set; }
-        public List<T> TrainSet { get; set; }
-        /// <summary>
-        /// numbers of neighbors
-        /// </summary>
-        public int K { get; set; }
+	public sealed class AlgorithmEngine<T, TClassifier> where T : IMetricable<T>, IClassifiable<TClassifier>
+	{
+		/// <summary>
+		/// indicates norm
+		/// </summary>
+		public double P
+		{
+			get; private set;
+		}
+		public List<T> TrainSet
+		{
+			get; private set;
+		}
+		/// <summary>
+		/// number of neighbors
+		/// </summary>
+		public int K
+		{
+			get; private set;
+		}
 
-        //TODO: check it
-        public List<T> KnnRun(List<T> testData)
-        {
-            foreach (var item in testData)
-                item.Classifier = GetMostCommonClassifier(GetKNeighbors(item));
+		public AlgorithmEngine(int k, double p, List<T> trainSet)
+		{
+			K = k;
+			P = p;
+			TrainSet = trainSet;
+		}
 
-            return testData;
-        }
+		public List<T> KnnRun(List<T> testData)
+		{
+			foreach (var item in testData)
+				item.Classifier = GetMostCommonClassifier(GetKNeighbors(item));
 
-        private TClassifier GetMostCommonClassifier(List<T> list)
-        {
-            if (list.Count > 0)
-                if (list[0].Classifier is ValueType)
-                    return GetMostCommonClassifier_ValueType(list);
+			return testData;
+		}
 
-            return GetMostCommonClassifier_RefType(list);
-        }
-        private TClassifier GetMostCommonClassifier_RefType(List<T> list)
-        {
-            throw new NotImplementedException();
-            return default(TClassifier);
-        }
-        private TClassifier GetMostCommonClassifier_ValueType(List<T> list)
-        {
-            Dictionary<TClassifier, int> counter = new Dictionary<TClassifier, int>();
-            TClassifier res = default(TClassifier);
-            int count = 0;
-            foreach (var item in list)
-            {
-                var key = item.Classifier;
-                counter[key]++;  //check if it not throws exception
-                if (count < counter[key])
-                {
-                    res = key;
-                    count = counter[key];
-                }
-            }
-           
-            return res;
-        }
-        private List<T> GetKNeighbors(T element)
-        {
-            SortedList<float, T> sortedList = new SortedList<float, T>(new DescFloatComparer());
-            foreach (var item in TrainSet)
-                sortedList.Add(item.NormP(element, P), item);
-            return sortedList.Values.Take(K).ToList();
-        }
-        class DescFloatComparer : IComparer<float>
-        {
-            public int Compare(float x, float y)
-            {
-                return Comparer<float>.Default.Compare(y, x);
-            }
-        }
-    }
+		private TClassifier GetMostCommonClassifier(List<T> list)
+		{
+			if (list.Count > 0)
+				if (list[0].Classifier is ValueType)
+					return GetMostCommonClassifier_ValueType(list);
+
+			return GetMostCommonClassifier_RefType(list);
+		}
+		private TClassifier GetMostCommonClassifier_RefType(List<T> list)
+		{
+			throw new NotImplementedException();
+			return default(TClassifier);
+		}
+		private TClassifier GetMostCommonClassifier_ValueType(List<T> list)
+		{
+			Dictionary<TClassifier, int> counter = new Dictionary<TClassifier, int>();
+			TClassifier res = default(TClassifier);
+			int count = 0;
+			foreach (var item in list)
+			{
+				var key = item.Classifier;
+				counter[key]++;  //check if it not throws exception
+				if (count < counter[key])
+				{
+					res = key;
+					count = counter[key];
+				}
+			}
+
+			return res;
+		}
+		private List<T> GetKNeighbors(T element)
+		{
+			SortedList<float, T> sortedList = new SortedList<float, T>();
+			foreach (var item in TrainSet) {
+				float distance = item.NormP(element, P);
+				if (sortedList.Count < K)
+					sortedList.Add(distance, item);
+				else if (sortedList.Max().Key > distance) {
+					sortedList.Remove(sortedList.Max().Key);
+					sortedList.Add(distance, item);
+				}
+			}
+			return sortedList.Values.ToList();
+		}
+
+	}
 }
