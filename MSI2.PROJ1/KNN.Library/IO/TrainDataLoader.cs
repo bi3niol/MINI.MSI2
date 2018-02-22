b4e02c820d21d863e97abbcffa5f4eaf1aeca2b4
Katarzyna Gołębiewska <staki777@wp.pl>
2018-02-22 13:56:42 +0100

    Console application; empty TrainDataLoader

---
+++ MSI2.PROJ1/KNN.Library/IO/TrainDataLoader.cs
@@ -0,0 +1,26 @@
+﻿using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KNN.Library.IO
+{
+	public class TrainDataLoader<T>
+	{
+		private string filePath;
+
+		public TrainDataLoader(string filePath)
+		{
+			this.filePath = filePath;
+		}
+
+		public List<T> LoadData()
+		{
+			List<T> trainData = new List<T>();
+#warning here data should be uploaded from csv file
+
+			return trainData;
+		}
+	}
+}
---
+++ MSI2.PROJ1/KNN.Library/ProblemEntities/Point.cs
@@ -0,0 +1,42 @@
+﻿using KNN.Library.Base;
+using System;
+using System.Collections.Generic;
+using System.Linq;
+using System.Text;
+using System.Threading.Tasks;
+
+namespace KNN.Project.Problem
+{
+	public class Point: IMetricable<Point>, IClassifiable<int>
+	{
+		public int Classifier
+		{
+			get; set;
+		}
+		public float X
+		{
+			get; private set;
+		}
+		public float Y
+		{
+			get; private set;
+		}
+
+		public Point(float x, float y)
+		{
+			X = x;
+			Y = y;
+			Classifier = -1;
+		}
+
+		public float NormP(Point other, double p)
+		{
+			if (p == double.PositiveInfinity)
+				return Math.Max(Math.Abs(X - other.X), Math.Abs(Y - other.Y));
+
+			float res = (float)Math.Pow(Math.Abs(X - other.X), p);
+			res += (float)Math.Pow(Math.Abs(Y - other.Y), p);
+			return (float)Math.Pow(res, 1.0 / p);
+		}
+	}
+}
