b01cff34b29a9407ec086d7e3c6e4632612fc381
Mateusz Bieńkowski <bi3niol@gmail.com>
2018-04-07 18:57:29 +0200

    test: jedna warstwa konwolucyjna + jedna dense - zmiana rozmiaru kernela

--- MSI2.PROJ2/CNNTester.py
+++ MSI2.PROJ2/CNNTester.py
@@ -17,7 +17,7 @@ class CNNTester(object):
     y_test = []
 
     def __init__(self):
-        if(len(CNNTester.X_train)== 0):
+        if(len(CNNTester.X_train) == 0):
             (X_train, y_train), (X_test, y_test) = mnist.load_data()
             X_train = X_train.reshape(X_train.shape[0], CNNTester.image_rows, CNNTester.image_cols, 1)
             X_test = X_test.reshape(X_test.shape[0], CNNTester.image_rows, CNNTester.image_cols, 1)
@@ -33,38 +33,44 @@ class CNNTester(object):
 
     def TestMethod(self):
         colNames = ['efficiency','accuracy',]
-        testResults =pd.DataFrame([],columns=colNames)
+        testResults = pd.DataFrame([],columns=colNames)
 
         layersSet = []
         model = self.GetModelTrained()
         return
+    def OneConvOneDense(self):
+        data = []
+        for x in range(3,28,4):
+            print('calculate for ',x)
+            model, tt,history = self.GetModelTrained([Conv2D(32, kernel_size=(x, x), activation='relu', input_shape=(28,28,1)),
+                        Flatten(),
+                        Dense(10, activation='softmax')],step=4,epochsM=6)
+            data.append({'time':tt,'itterations':6,'acc':history.history['acc'],'kernelSize':x})
+            df = pd.DataFrame(data)
+            print('saving result')
+            df.to_csv("./OneConvOneDense.csv",';')
+            print('saved to OneConvOneDense.csv')
+        pass
 
-    def GetModelTrained(self,layers,lossM=keras.losses.categorical_crossentropy,optM=keras.optimizers.Adadelta,metricsM=['accuracy'],epochsM=1,batch_sizeM=128):
+    def GetModelTrained(self,layers,lossM=keras.losses.categorical_crossentropy,optM=keras.optimizers.Adadelta,metricsM=['accuracy'],epochsM=1,batch_sizeM=128,step=1):
         model = Sequential(layers)
 
         model.compile(loss=lossM,
               optimizer=optM(),
               metrics=metricsM)
-
-        model.fit(CNNTester.X_train,
-          CNNTester.y_train,
+        import time
+        ct = time.time()
+        history = model.fit(CNNTester.X_train[0:len(CNNTester.X_train):step],
+          CNNTester.y_train[0:len(CNNTester.y_train):step],
           batch_size=batch_sizeM,
           epochs=epochsM,
-          verbose=1)
+          verbose=1,
+          validation_data=(CNNTester.X_test,CNNTester.y_test))
+        tt = time.time() - ct
+        return model, tt, history
 
-        return model
-
-if(__name__=="__main__"):
-    tester = CNNTester()
-    model = tester.GetModelTrained([Conv2D(32, kernel_size=(28, 28), activation='relu', input_shape=(28,28,1)),
-                    #Conv2D(64, (3, 3), activation='relu'),
-                    #MaxPooling2D(pool_size=(2, 2)),
-                    #Dropout(0.2),   
-                    Flatten(),
-                    #Dense(128, activation='relu'),
-                    #Dropout(0.5),
-                    Dense(10, activation='softmax')])
-    score = model.evaluate(CNNTester.X_test,CNNTester.y_test, verbose=0)
-    print('loss:', score[0])
-    print('accuracy:', score[1])
-    print(score)
+if(__name__ == "__main__"):
+    print('sadw')
+    train = CNNTester()
+    train.OneConvOneDense()
+    
