15b5da939dd3d6b7478588576c70a54bb2b96b63
Mateusz Bieńkowski <bi3niol@gmail.com>
2018-04-17 17:02:44 +0200

    mozna zapuszczac testy

--- MSI2.PROJ2/SVM/classifier.py
+++ MSI2.PROJ2/SVM/classifier.py
@@ -36,9 +36,9 @@ URL = "http://mlr.cs.umass.edu/ml/machine-learning-databases/spambase/spambase.d
 # rather than displaying interactive UI.
 #import matplotlib
 #matplotlib.use('Agg')
-
 import pandas as pd
 from pandas import read_table
+import pandas as pd
 import numpy as np
 import matplotlib.pyplot as plt
 import keras
@@ -51,19 +51,15 @@ except ImportError:
     pass
 
 # =====================================================================
-
 def download_data_sets():
-    '''
-    Downloads the data for this script into a pandas DataFrame.
-    '''
     (X_train, y_train), (X_test, y_test) = mnist.load_data()
-    X_train = X_train.reshape(X_train.shape[0],28*28)
-    X_test = X_test.reshape(X_test.shape[0],28*28)
+    X_train = X_train.reshape(X_train.shape[0],28 * 28)
+    X_test = X_test.reshape(X_test.shape[0],28 * 28)
 
     X_train = X_train.astype('float32')
     X_test = X_test.astype('float32')
-    X_train[X_train>0]=1
-    X_test[X_test>0]=1
+    X_train[X_train > 0] = 1
+    X_test[X_test > 0] = 1
 
     print(X_train.shape)
     print(y_test.shape)
@@ -75,89 +71,44 @@ def download_data_sets():
 
 
 # =====================================================================
-
 def evaluate_classifier(X_train, X_test, y_train, y_test):
-    '''
-    Run multiple times with different classifiers to get an idea of the
-    relative performance of each configuration.
-
-    Returns a sequence of tuples containing:
-        (title, precision, recall)
-    for each learner.
-    '''
-
-    # Import some classifiers to test
     from sklearn.svm import LinearSVC, NuSVC, SVC
     from sklearn.ensemble import AdaBoostClassifier
 
-    # We will calculate the P-R curve for each classifier
     from sklearn.metrics import precision_recall_curve, f1_score
     from sklearn import metrics
-    # Here we create classifiers with default parameters. These need
-    # to be adjusted to obtain optimal performance on your data set.
-    
 
-    classifier = SVC(C=1,gamma=0.002)
-    # uczenie
-    classifier.fit(X_train, y_train)
+    def GetClassifiers():
+        for c in [1,2,3,4,5,6]:
+            for g in [1,4,7,10,14]:
+                yield (c/5,g/1000,SVC(C=c,gamma=g))
 
-    expected = y_test
-    predicted = classifier.predict(X_test)
-    
-    score = f1_score(expected, predicted,average=None)  
-    # Generate the P-R curve
-    y_prob = classifier.decision_function(X_test)
-    precision, recall, _ = metrics.classification_report(y_test, y_prob)
-    yield 'Linear SVC (F1 score={:.3f})'.format(score), precision, recall
+    def AccuracyFunc(orgin,predic):
+        tmp = orgin == predic
+        res = np.sum(tmp) / len(orgin) * 100
+        return res
 
-    # Test the linear support vector classifier
-    classifier = LinearSVC(C=1)
-    # Fit the classifier
-    classifier.fit(X_train, y_train)
-    score = f1_score(y_test, classifier.predict(X_test),average=None)
-    # Generate the P-R curve
-    y_prob = classifier.decision_function(X_test)
-    precision, recall, _ = metrics.classification_report(y_test, y_prob)
-    # Include the score in the title
-    yield 'Linear SVC (F1 score={:.3f})'.format(score), precision, recall
+    import time
 
-    # Test the Nu support vector classifier
-    classifier = NuSVC(kernel='rbf', nu=0.5, gamma=1e-3,)
-    # Fit the classifier
-    classifier.fit(X_train, y_train)
-    score = f1_score(y_test, classifier.predict(X_test),average=None)
-    # Generate the P-R curve
-    y_prob = classifier.decision_function(X_test)
-    precision, recall, _ = metrics.classification_report(y_test, y_prob)
-    # Include the score in the title
-    yield 'NuSVC (F1 score={:.3f})'.format(score), precision, recall
+    res = []
+    FileName = str.format('SVM_test_{}.csv', time.time())
+    for (c, g, classifier) in GetClassifiers():
+        # Fit the classifier
+        print(str.format('C = {} G = {} classifier = {} ',c,g,classifier))
+        startTime = time.time()
+        classifier.fit(X_train, y_train)
+        tt = time.time() - startTime
 
-    # Test the Ada boost classifier
-    classifier = AdaBoostClassifier(n_estimators=50, learning_rate=1.0, algorithm='SAMME.R')
-    # Fit the classifier
-    classifier.fit(X_train, y_train)
-    score = f1_score(y_test, classifier.predict(X_test),average=None)
-    # Generate the P-R curve
-    y_prob = classifier.decision_function(X_test)
-    precision, recall, _ = metrics.classification_report(y_test, y_prob)
-    # Include the score in the title
-    yield 'Ada Boost (F1 score={:.3f})'.format(score), precision, recall
+        predicted = classifier.predict(X_test)
+        accuracy = AccuracyFunc(y_test,predicted)
+        res.append({"C":c,"G":g,"Accuracy":accuracy,"Time":tt})
 
-# =====================================================================
-
-
+        df = pd.DataFrame(data)
+        print('Saving results...')
+        df.to_csv("./" + FileName,';')
+        print('Results saved to ',FileName)
+        # =====================================================================
 def plot(results):
-    '''
-    Create a plot comparing multiple learners.
-
-    `results` is a list of tuples containing:
-        (title, precision, recall)
-    
-    All the elements in results will be plotted.
-    '''
-
-    # Plot the precision-recall curves
-
     fig = plt.figure(figsize=(6, 6))
     fig.canvas.set_window_title('Classifying data from ' + URL)
 
@@ -190,24 +141,16 @@ def plot(results):
     #img_stream = BytesIO()
     #plt.savefig(img_stream, fmt='png')
     #img_bytes = img_stream.getvalue()
-    #print('Image is {} bytes - {!r}'.format(len(img_bytes), img_bytes[:8] + b'...'))
+    #print('Image is {} bytes - {!r}'.format(len(img_bytes), img_bytes[:8] +
+    #b'...'))
 
     # Closing the figure allows matplotlib to release the memory used.
     plt.close()
 
 
 # =====================================================================
-
-
 if __name__ == '__main__':
 
-    # Download the data set from URL
     (X_train, y_train), (X_test, y_test) = download_data_sets()
-
-    # Evaluate multiple classifiers on the data
     print("Evaluating classifiers")
-    results = list(evaluate_classifier(X_train, X_test, y_train, y_test))
-
-    # Display the results
-    print("Plotting the results")
-    plot(results)
+    evaluate_classifier(X_train, X_test, y_train, y_test)
