8b17794a0819fa86201f45e91b2f58b496894983
Mateusz Bieńkowski <bi3niol@gmail.com>
2018-05-28 19:24:20 +0200

    wyniki testów dla wybranych najlepszych modeli

--- MSI2.PROJ2/CNN/CNNTester.py
+++ MSI2.PROJ2/CNN/CNNTester.py
@@ -12,8 +12,8 @@ class CNNTester:
         if model == None:
             print("invalid model {}",modelName)
             return
-        history, duration = CNN.CNN.RunCNN(reader, model, 5)
-        data.append({'model': modelName, 'iterations': 5, 'time': round(duration, 2),'acc': [ round(elem, 4) for elem in history['acc']],
+        history, duration = CNN.CNN.RunCNN(reader, model, 20)
+        data.append({'model': modelName, 'iterations': 20, 'time': round(duration, 2),'acc': [ round(elem, 4) for elem in history['acc']],
                     'loss': [ round(elem, 4) for elem in history['loss']], 'val_acc': [ round(elem, 4) for elem in history['val_acc']],'val_loss': [ round(elem, 4) for elem in history['val_loss']]})
 
         df = pd.DataFrame(data)
@@ -22,26 +22,33 @@ class CNNTester:
         print('Results saved to ',FileName)
 
     if __name__ == '__main__':
-        fileName = str.format('CNN_test_{}.csv', time.time())
+        fileName = 'CNN_Model4.csv'#str.format('CNN_test_{}.csv', time.time())
         data = []
         reader = MNISTReader.MNISTReader()
-        for k in range(2,20,5):
-            for p in range(2,3):
-                for d in range(1,6):
-                    testModel(reader, NetworkModels.model1(reader.input_shape, reader.num_classes, (k, k), (p, p), d / 10), str.format("model1, kernel: ({},{}), pool: ({}, {}), dropout: {} ", k, k, p, p, d / 10), data)
-        for k in range(2,20,5):
-            for p in range(2,3):
-                testModel(reader, NetworkModels.model2(reader.input_shape, reader.num_classes, (k, k), (p, p)), str.format("model2, kernel: ({},{}), pool: ({}, {}) ", k, k, p, p), data)
+        #testModel(reader, NetworkModels.model1(reader.input_shape, reader.num_classes, (7,7), (2,2), 0.1), str.format("model1, kernel: ({},{}), pool: ({}, {}), dropout: {} ", 7, 7, 2, 2, 0.1), data)
+        #testModel(reader, NetworkModels.model1(reader.input_shape, reader.num_classes, (7,7), (2,2), 0.2), str.format("model1, kernel: ({},{}), pool: ({}, {}), dropout: {} ",7, 7, 2, 2, 0.2), data)
+        #testModel(reader, NetworkModels.model1(reader.input_shape, reader.num_classes, (2,2), (2,2), 0.2), str.format("model1, kernel: ({},{}), pool: ({}, {}), dropout: {} ", 2, 2, 2, 2, 0.2), data)
+        testModel(reader, NetworkModels.model4(reader.input_shape, reader.num_classes, (7,7), (7,7)), str.format("model4, kernel1: ({},{}), kernel2:({}, {})", 7,7,7,7), data,fileName)
+        testModel(reader, NetworkModels.model4(reader.input_shape, reader.num_classes, (7,7), (2,2)), str.format("model4, kernel1: ({},{}), kernel2:({}, {})", 7,7,2,2), data,fileName)
+        testModel(reader, NetworkModels.model4(reader.input_shape, reader.num_classes, (12,12), (2,2)), str.format("model4, kernel1: ({},{}), kernel2:({}, {})", 12,12,2,2), data,fileName)
+
+        #for k in range(2,20,5):
+        #    for p in range(2,3):
+        #        for d in range(1,6):
+        #            testModel(reader, NetworkModels.model1(reader.input_shape, reader.num_classes, (k, k), (p, p), d / 10), str.format("model1, kernel: ({},{}), pool: ({}, {}), dropout: {} ", k, k, p, p, d / 10), data)
+        #for k in range(2,20,5):
+        #    for p in range(2,3):
+        #        testModel(reader, NetworkModels.model2(reader.input_shape, reader.num_classes, (k, k), (p, p)), str.format("model2, kernel: ({},{}), pool: ({}, {}) ", k, k, p, p), data)
         
-        for k in range(2,20,5):
-            testModel(reader, NetworkModels.model3(reader.input_shape, reader.num_classes, (k, k)), str.format("model3, kernel: ({},{})", k, k), data)
-        for k1 in range(2,20,5):
-            for k2 in range(2,20,5):
-                    testModel(reader, NetworkModels.model4(reader.input_shape, reader.num_classes, (k1, k1), (k2, k2)), str.format("model4, kernel1: ({},{}), kernel2:({}, {})", k1, k1, k2, k2), data)
-        for k in range(12,20,5):
-            for p in range(2,3):
-                for d in range (1,6):
-                    testModel(reader, NetworkModels.model5(reader.input_shape, reader.num_classes, (k, k), (p, p), d/10  ), str.format("model5, kernel: ({},{}), pool: ({}, {}), dropout: {} ", k, k, p, p, d/10), data)
+        #for k in range(2,20,5):
+        #    testModel(reader, NetworkModels.model3(reader.input_shape, reader.num_classes, (k, k)), str.format("model3, kernel: ({},{})", k, k), data)
+        #for k1 in range(2,20,5):
+        #    for k2 in range(2,20,5):
+        #            testModel(reader, NetworkModels.model4(reader.input_shape, reader.num_classes, (k1, k1), (k2, k2)), str.format("model4, kernel1: ({},{}), kernel2:({}, {})", k1, k1, k2, k2), data)
+        #for k in range(12,20,5):
+        #    for p in range(2,3):
+        #        for d in range (1,6):
+        #            testModel(reader, NetworkModels.model5(reader.input_shape, reader.num_classes, (k, k), (p, p), d/10  ), str.format("model5, kernel: ({},{}), pool: ({}, {}), dropout: {} ", k, k, p, p, d/10), data)
         df = pd.DataFrame(data)
         print('Saving results...')
         df.to_csv("./" + fileName,';')
